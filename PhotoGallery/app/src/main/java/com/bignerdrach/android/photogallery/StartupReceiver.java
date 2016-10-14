792de3e3aed167fe2338a24b86071eb6352e4c8c
Mike Wasauskee <m68476521.1@gmail.com>
2016-10-13 21:02:07 -0700

    feat: finishing notifications

---
+++ PhotoGallery/app/src/main/java/com/bignerdrach/android/photogallery/StartupReceiver.java
@@ -0,0 +1,24 @@
+package com.bignerdrach.android.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by mike on 10/12/16.
+ * This class is use to handle standalone receiver like a broadcast receiver.
+ * It should be declared in the manifest
+ */
+
+public class StartupReceiver extends BroadcastReceiver {
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+
+        boolean isOn = QueryPreferences.isAlarmOn(context);
+        PollService.setServiceAlarm(context, isOn);
+    }
+}
