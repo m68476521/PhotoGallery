7968ad1ed3cef3ff9542322d3c9da6085f2f4e20
Mike Wasauskee <m68476521.1@gmail.com>
2016-10-15 20:18:02 -0700

    feat: adding a progress bar

--- PhotoGallery/app/src/main/java/com/bignerdrach/android/photogallery/PhotoPageFragment.java
+++ PhotoGallery/app/src/main/java/com/bignerdrach/android/photogallery/PhotoPageFragment.java
@@ -2,11 +2,14 @@ package com.bignerdrach.android.photogallery;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 /**
  * Created by mike on 10/15/16.
@@ -17,6 +20,7 @@ public class PhotoPageFragment extends VisibleFragment {
 
     private Uri mUri;
     private WebView mWebView;
+    private ProgressBar mProgressBar;
 
     public static PhotoPageFragment newInstance(Uri uri) {
         Bundle args = new Bundle();
@@ -39,9 +43,27 @@ public class PhotoPageFragment extends VisibleFragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_photo_page, container, false);
 
-        mWebView = (WebView) v.findViewById(R.id.fragment_photo_page_web_view);
+        mProgressBar = (ProgressBar) v.findViewById(R.id.fragment_photo_page_progress_bar);
 
+        mProgressBar.setMax(100); //webChromeClient reports in range 0-100
+
+        mWebView = (WebView) v.findViewById(R.id.fragment_photo_page_web_view);
         mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView webView, int newProgress) {
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+
+            public void onReceivedTitle(WebView webView, String title) {
+                AppCompatActivity activity = (AppCompatActivity) getActivity();
+                activity.getSupportActionBar().setSubtitle(title);
+            }
+        });
         mWebView.setWebViewClient(new WebViewClient() {
             public boolean shouldOverrideUrlLoagding(WebView view, String url) {
                 return false;
